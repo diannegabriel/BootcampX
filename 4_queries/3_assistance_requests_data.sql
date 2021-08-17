aac06fecef9a6c4b8b9e085b1a861579a86b2b86
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 11:05:13 -0400

    Fix mistake in 3_assistance_request_data. Wrong join id

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -5,5 +5,5 @@ SELECT teachers.name AS teacher,
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
-JOIN assignments ON assignment_id = students.id
+JOIN assignments ON assignment_id = assignments.id
 ORDER BY duration;
