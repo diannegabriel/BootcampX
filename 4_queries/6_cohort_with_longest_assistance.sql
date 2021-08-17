560ebb6b3fc8932326e5d372a0d17d4574cf18c3
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 13:16:26 -0400

    Implement 6_cohort_with_longest_assistance.sql

---
+++ 4_queries/6_cohort_with_longest_assistance.sql
@@ -0,0 +1,13 @@
+SELECT cohorts.name AS name,
+AVG(completed_at - started_at) AS average_assistance_request_duration
+FROM assistance_requests
+JOIN students ON student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE AVG(completed_at - started_at) > (
+  SELECT average_assistance_request_duration
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  GROUP BY cohorts.name
+)
+GROUP BY cohorts.name;
