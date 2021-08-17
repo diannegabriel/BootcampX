144cf8aaf2dac5836e0b23d4894751631c096fdb
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 13:28:29 -0400

    Update with the correct answer 8_cohort_average_assistance_duration.sql

--- 4_queries/8_cohort_average_assistance_duration.sql
+++ 4_queries/8_cohort_average_assistance_duration.sql
@@ -1,7 +1,19 @@
-SELECT AVG(completed_at - started_at) AS average_total_duration 
-FROM assistance_requests
-JOIN students ON student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
+-- Original answer (wrong):
+-- SELECT AVG(completed_at - started_at) AS average_total_duration 
+-- FROM assistance_requests
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON students.cohort_id = cohorts.id
 -- GROUP BY cohorts.name
 -- HAVING AVG(SUM(completed_at - started_at));
 -- ORDER BY average_total_duration;
+
+-- Compass answer:
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+  SELECT cohorts.name AS cohort, SUM(completed_at-started_at) AS total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS total_durations;
