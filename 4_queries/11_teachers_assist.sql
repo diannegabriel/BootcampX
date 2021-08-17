b00265f529bbfa3de2c39b491fc3e4868ec05e94
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 14:00:34 -0400

    Implement 11_teachers_assist.sql

---
+++ 4_queries/11_teachers_assist.sql
@@ -0,0 +1,9 @@
+-- Get the name of all teachers that performed an assistance request during a cohort.
+SELECT DISTINCT teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name;
