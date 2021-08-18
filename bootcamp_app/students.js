643215b4719160d909b7a1e406e099672db49871
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 21:53:56 -0400

    Test node postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+});
