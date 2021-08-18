46256d35f9c3de58c92b48b5507ce72190f8a6da
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 22:33:54 -0400

    Update teachers.js with Compass answer

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,20 +7,37 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// Original answer:
+// pool.query(`
+// SELECT DISTINCT teachers.name AS teacher,
+//   cohorts.name AS cohort,
+//   COUNT(*) AS total_assistances
+// FROM assistance_requests
+// JOIN teachers ON teachers.id = teacher_id
+// JOIN students ON students.id = student_id
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// GROUP BY teachers.name, cohorts.name
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.cohort}: ${user.teacher}`)
+//   })
+// });
+
+// Compass answer:
 pool.query(`
-SELECT DISTINCT teachers.name AS teacher,
-  cohorts.name AS cohort,
-  COUNT(*) AS total_assistances
-FROM assistance_requests
-JOIN teachers ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-GROUP BY teachers.name, cohorts.name
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`)
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
