b4e9710ab38a777d43aa8843f5b18a019ea381c5
Dianne <mdianne.gabriel@gmail.com>
2021-08-17 21:57:30 -0400

    Modify students.js to take process.argv

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,11 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+// pool.query(`
+// SELECT students.id, students.name, cohorts.name AS cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+
 pool.query(`
 SELECT students.id, students.name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3]};
 `)
 .then(res => {
   res.rows.forEach(user => {
